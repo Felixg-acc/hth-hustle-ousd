e30ed9c90619b08f54d0699d73c568f78bac9c3e
felix gumataotao <felixgumataotao@felixs-MBP.attlocal.net>
2025-06-18 12:16:42 -0700

    lab2

---
+++ lab2.py
@@ -0,0 +1,22 @@
+name = "felix gumataotao"
+
+nickname = "fefe"
+
+age = 16
+
+used_before = False
+
+hobbies = ["sports", "card tricks", "eating", "sleeping", "socializing"]
+
+my_favs = {'food': ['salad', 'pizza'],'anime': ['hunter x hunter', 'attack on titan']}
+
+teachers = {"parkerb","parkerb", "mia" }
+
+teachers1 = ("parkerb ", "parker b", "apple", "mia")
+
+print(name)
+print(age)
+print(hobbies)
+
+
+
