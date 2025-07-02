d3bb9af00bdfbb069743666f2ce3d3923f9d6f12
Felixg-acc <felixgumataotao654@gmail.com>
2025-07-02 14:38:38 -0700

    Add files via upload

---
+++ lab3.py
@@ -0,0 +1,20 @@
+# Lab 3
+
+food = 'pizza'
+#print(food[:3])
+#print(food[-3:])
+first_last = food[0] + food[-1]
+#print(first_last)
+food_list = food.split()
+#print(food_list)
+joined_food = ' '.join(food_list)
+#print(joined_food)
+
+# Task 2: working with lists
+number_list = [1, 7675647456, 32, -5, 0, 234]
+number_list.append(67)
+#print(number_list)
+number_list.insert(3, 1000)
+#print(number_list)
+number_list.pop()
+#print(number_list)
